db37d43c13198321d323beeff0dbe50ed2f5500a
Arnaud Delante <contact@arnaud.ninja>
2025-12-16 16:34:51 +0100

    Add a basic select

--- components/ressource-form.tsx
+++ components/ressource-form.tsx
@@ -4,6 +4,13 @@ import { useRouter } from "next/navigation"
 import { Field, FieldLabel, FieldGroup } from "./ui/field"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default function RessourceForm() {
   const router = useRouter()
@@ -20,6 +27,28 @@ export default function RessourceForm() {
             placeholder="Carinite (Pure)"
             required
           />
+        </Field>
+
+        <Field>
+          <FieldLabel htmlFor="type">
+            Type
+          </FieldLabel>
+          <Select defaultValue="">
+            <SelectTrigger id="type">
+              <SelectValue placeholder="Other" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="Armor">Armor</SelectItem>
+              <SelectItem value="Clothing">Clothing</SelectItem>
+              <SelectItem value="Weapons">Weapons</SelectItem>
+              <SelectItem value="Utility">Utility</SelectItem>
+              <SelectItem value="Ammo">Ammo</SelectItem>
+              <SelectItem value="Vehicles">Vehicles</SelectItem>
+              <SelectItem value="Sustenance">Sustenance</SelectItem>
+              <SelectItem value="Container">Container</SelectItem>
+              <SelectItem value="Other">Other</SelectItem>
+            </SelectContent>
+          </Select>
         </Field>
 
         <Field>
